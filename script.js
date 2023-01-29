9fdd177fd3a34b87fc38ac243392aa38e33e461a
arjundevlop <109807311+arjundevlop@users.noreply.github.com>
2023-01-29 20:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+function addtodo(){
+    var li = document.createElement("li");
+    var inputvalue = document.getElementById("newTodoInput").value;
+    var t = document.createTextNode(inputvalue);
+    li.appendChild(t);
+    if(inputvalue==='')
+    {
+        alert("must write something");
+    } else {
+        document.getElementById("todoList").appendChild(li);
+    }
+    document.getElementById("newTodoInput").value = "";
+}
